fce5432ae9e0b2b82b3b992a27ba32e2cf39830f
Matthias Schobner <matthias.schobner@me.com>
2018-11-15 15:30:09 +0100

    config GuzzleBundle

--- DependencyInjection/GonettoFCApiClientExtension.php
+++ DependencyInjection/GonettoFCApiClientExtension.php
@@ -2,28 +2,62 @@
 
 namespace Gonetto\FCApiClientBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration.
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class GonettoFCApiClientExtension extends Extension
+class GonettoFCApiClientExtension extends Extension implements PrependExtensionInterface
 {
+
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        // Get all bundles
+        $bundles = $container->getParameter('kernel.bundles');
+
+        // Config EightPointsGuzzleBundle
+        if (isset($bundles['EightPointsGuzzleBundle'])) {
+            $container->prependExtensionConfig(
+              'eight_points_guzzle',
+              [
+                'logging' => true,
+                'clients' => [
+                  'finance_consult' => [
+                    'base_url' => '%gonetto_fc_api_client.base_api_url%',
+                    'options' => [
+                      'headers' => [
+                        'Accept' => 'application/json',
+                      ],
+                    ],
+                  ],
+                ],
+              ]
+            );
+        }
+    }
+
     /**
      * {@inheritdoc}
+     *
+     * @param array $configs
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     *
+     * @throws \Exception
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        // Load bundle configs
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('config.yml');
         $loader->load('services.yml');
     }
 }
-
