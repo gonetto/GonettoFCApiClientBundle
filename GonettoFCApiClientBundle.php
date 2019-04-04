3352167bdc840e50ce2224edc5ce0cb92b8e1c0e
Matthias Schobner <matthias.schobner@me.com>
2019-04-04 11:52:08 +0200

    drupal dependencies bugfix

--- GonettoFCApiClientBundle.php
+++ GonettoFCApiClientBundle.php
@@ -2,8 +2,13 @@
 
 namespace Gonetto\FCApiClientBundle;
 
+use Gonetto\FCApiClientBundle\DependencyInjection\GonettoFCApiClientExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class GonettoFCApiClientBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new GonettoFCApiClientExtension();
+    }
 }
