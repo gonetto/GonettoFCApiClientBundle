fc756cc2e950b6e147a7449fb53643d0645c8c0a
Matthias Schobner <matthias.schobner@me.com>
2019-04-09 15:20:30 +0200

    add customer update: add Finance Consult Response Class Schema

---
+++ Model/CustomerUpdateResponse.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Gonetto\FCApiClientBundle\Model;
+
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Class DataResponse
+ *
+ * @package Gonetto\FCApiClientBundle\Model
+ */
+class CustomerUpdateResponse
+{
+
+    /**
+     * @var boolean
+     *
+     * @JMS\Type("boolean")
+     * @JMS\SerializedName("result")
+     */
+    protected $success;
+
+    /**
+     * @var string
+     *
+     * @JMS\Type("string")
+     * @JMS\SerializedName("error")
+     */
+    protected $errorMessage;
+
+    /**
+     * @return bool
+     */
+    public function isSuccess(): bool
+    {
+        return $this->success;
+    }
+
+    /**
+     * @param bool $success
+     *
+     * @return CustomerUpdateResponse
+     */
+    public function setSuccess(bool $success): CustomerUpdateResponse
+    {
+        $this->success = $success;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getErrorMessage(): ?string
+    {
+        return $this->errorMessage;
+    }
+
+    /**
+     * @param string $errorMessage
+     *
+     * @return CustomerUpdateResponse
+     */
+    public function setErrorMessage(string $errorMessage): CustomerUpdateResponse
+    {
+        $this->errorMessage = $errorMessage;
+
+        return $this;
+    }
+}
